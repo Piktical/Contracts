e090ae3c753484972958a10a7b187302c3a5401f
Rob Lyndon <rob.lyndon@gmail.com>
2019-02-17 10:42:35 +0000

    Manual registration model

---
+++ src/Piktical.Api.EventSecurity/Domain.fs
@@ -0,0 +1,13 @@
+﻿namespace Piktical.Api.EventSecurity
+
+module Domain =
+    open System
+    open Piktical.Api.Core.Domain
+
+    type ManualRegistration =
+        {
+            RegistrationId: Guid
+            TicketId: Guid
+            HolderId: Guid
+            ImageUrl: string
+        }
