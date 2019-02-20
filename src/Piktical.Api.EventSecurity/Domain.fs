b8bfa75fd0ee4689b03a65b66296be2dfd681a16
Rob Lyndon <rob.lyndon@gmail.com>
2019-02-20 13:29:37 +0000

    Entrance type added

--- src/Piktical.Api.EventSecurity/Domain.fs
+++ src/Piktical.Api.EventSecurity/Domain.fs
@@ -4,10 +4,17 @@ module Domain =
     open System
     open Piktical.Api.Core.Domain
 
-    type ManualRegistration =
-        {
-            RegistrationId: Guid
-            TicketId: Guid
-            HolderId: Guid
-            ImageUrl: string
-        }
+    type ManualRegistration = {
+        RegistrationId: Guid
+        TicketId: Guid
+        HolderId: Guid
+        ImageUrl: string
+    }
+
+    type Entrance = {
+        EntranceId: Guid
+        TicketHolderId: Guid
+        TicketOrderId: Guid
+        ApprovedUtc: DateTime
+        ApprovedById: Guid
+    }
